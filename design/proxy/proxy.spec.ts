5b75e93d3310651add7efa0f5895bf588bec2153
dalcon10028 <dalcon10280@gmail.com>
2022-06-06 21:45:32 +0900

    proxy

---
+++ design/proxy/proxy.spec.ts
@@ -0,0 +1,13 @@
+import YeonProxy from "./proxy";
+
+it('이름은 "이연권" 입니다.', () => {
+    expect(YeonProxy.name).toBe('이연권');
+});
+
+it('국적은 한국인 입니다.', () => {
+    expect(YeonProxy.nationality).toBe('Korean');
+});
+
+it('나이는 25입니다.', () => {
+    expect(YeonProxy.age).toBe(25);
+});
