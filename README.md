3f36bd2fae5c70b47dc39c91fe1d0afdcb1c6a0c
dalcon10028 <dalcon10280@gmail.com>
2022-06-27 00:10:56 +0900

    README.md 작성

---
+++ README.md
@@ -0,0 +1,13 @@
+# Patterns
+
+[Patterns](https://www.patterns.dev/) 사이트를 참고하여 작성한 예제 및 테스트 코드
+
+## Setting
+```bash
+yarn install
+```
+
+## Testing
+```bash
+yarn test
+```
--- design/proxy/proxy.ts
+++ design/proxy/proxy.ts
@@ -11,15 +11,15 @@ const person: Person = {
 };
 
 const personProxy = new Proxy(person, {
-    get: (obj: Person, prop: keyof Person) => {
+    get: (obj: Person, prop: keyof Person, receiver) => {
         /* https://ui.toast.com/weekly-pick/ko_20210413 */
         console.log(`${prop}의 값은 ${Reflect.get(obj, prop)}입니다.`);
-        return obj[prop];
+        return Reflect.get(obj, prop, receiver);
     },
     /* https://github.com/microsoft/TypeScript/issues/47295 */
-    set: (obj: Person, prop: keyof Person, value) => {
+    set: (obj: Person, prop: keyof Person, value, receiver) => {
         console.log(`${prop}의 값이 ${obj[prop]}에서 ${value}로 변경되었습니다.`);
-        return Reflect.set(obj, prop, value);
+        return Reflect.set(obj, prop, value, receiver);
     }
 });
 
